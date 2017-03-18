0a9197bb559e4f288c6d9684c3bb391fe06b8151
Sebastian Trześniewski <sembsa@me.com>
2017-03-18 18:03:17 +0100

    Dodanie serwera

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import SimpleHTTPServer
+import SocketServer
+
+hostname = "localhost"
+port = 80
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer((hostname, port), Handler)
+
+print "Server starts: ", port
+
+httpd.serve_forever()
